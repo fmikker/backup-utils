15e1e7a8999979ab1356760847f577b962ede2a5
Fredrik Mikker <fredrik@mikker.se>
2017-03-15 14:45:22 +0100

    Added excludes file function
    This enables the user to use a exclude file located in the homedir
    to skip some paths that should not be backed up.
    
    Signed-off-by: Fredrik Mikker <fredrik@mikker.se>

--- borgbackup.sh
+++ borgbackup.sh
@@ -1,19 +1,37 @@
 #!/bin/sh
-REPOSITORY=/path/to/location
+# Borg cron backup script with desktop notifications
+# Meant to be run as root
+#
+# Copyright (C) 2016 Olivier Bilodeau
+# Licensed under the MIT License
 
-# Backup all of /home and /var/www except a few
-# excluded directories
+# configuration
+USER=
+REPOSITORY=
+COMPRESSION=zlib,5
+EXCLUDES=/home/user/.borgexcludes
+# derived information
+export BORG_CACHE_DIR=/home/$USER/.cache/borg/
+USERID=`id -u $USER`
+
 borg create -v --stats                          \
+    --compression $COMPRESSION                  \
     $REPOSITORY::'{hostname}-{now:%Y-%m-%d}'    \
-    /home                                       \
-    /var/www                                    \
-    --exclude '/home/*/.cache'                  \
-    --exclude '/home/Downloads'					\
-    --exclude '*.pyc'
+    /home/$USER/ 								\
+	--exclude-from $EXCLUDES
+
+if [[ $? == 0 ]]; then
+        MSG="Backup success"
+        ICON=document-send
+else
+        MSG="Backup failed... See root's dead.letter for details."
+        ICON=dialog-warning
+fi
+sudo -u $USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$USERID/bus notify-send 'BorgBackup' "$MSG" --icon=$ICON
 
 # Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
 # archives of THIS machine. The '{hostname}-' prefix is very important to
 # limit prune's operation to this machine's archives and not apply to
 # other machine's archives also.
-borg prune -v --list $REPOSITORY --prefix '{hostname}-' \
-    --keep-daily=7 --keep-weekly=4 --keep-monthly=6
+borg prune -v $REPOSITORY --prefix '{hostname}-' \
+--keep-daily=7 --keep-weekly=4 --keep-monthly=6
